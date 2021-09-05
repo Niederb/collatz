03f3ee1172fd9e5faab77eeec23b8256b039a2c8
Thomas Niederberger <thomas@niederb.ch>
2021-09-05 16:21:04 +0200

    Improve examples

--- examples/get_steps.rs
+++ examples/get_steps.rs
@@ -4,10 +4,23 @@ use std::time::Instant;
 
 fn main() {
     let end = 10_000_000;
+
+    let mut s: Vec<u128> = (1..=end).collect();
+    let now = Instant::now();
+    s.iter_mut()
+        .for_each(|p| *p = collatz::get_steps(*p as u128).len() as u128);
+    println!("Time single threaded: {}ms", now.elapsed().as_millis());
+
     let mut r: Vec<u128> = (1..=end).collect();
     let now = Instant::now();
     r.par_iter_mut()
         .for_each(|p| *p = collatz::get_steps(*p as u128).len() as u128);
+    println!("Time with rayon: {}ms", now.elapsed().as_millis());
 
-    println!("{}", now.elapsed().as_millis());
+    // Verify the results are the same
+    for (i, val1) in s.iter().enumerate() {
+        if *val1 != r[i] {
+            panic!("Values should be the same!");
+        }
+    }
 }
